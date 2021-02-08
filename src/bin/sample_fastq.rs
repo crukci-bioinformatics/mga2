5be8273fa5b700057d30af8a1a2e66cce9b43ae3
eldrid01 <matthew.d.eldridge@gmail.com>
2021-02-08 13:50:41 +0000

    Added utilties for sampling FASTQ records and combining, trimming and splitting FASTQ records.

---
+++ src/bin/sample_fastq.rs
@@ -0,0 +1,210 @@
+use anyhow::{Context, Result};
+use log::info;
+use mga::fastq::{create_fastq_reader, create_fastq_writer, FastqRecord};
+use rand::{thread_rng, Rng};
+use serde::Serialize;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// Sample a specified number of records from a FASTQ file.
+#[derive(StructOpt)]
+struct Config {
+    /// Identifier for dataset
+    #[structopt(short, long)]
+    id: Option<String>,
+
+    /// FASTQ file to sample records from
+    #[structopt(parse(from_os_str))]
+    fastq_files: Vec<PathBuf>,
+
+    /// FASTQ file to which sampled records will be written
+    #[structopt(short, long)]
+    output_file: Option<PathBuf>,
+
+    /// The number of records to sample
+    #[structopt(short = "n", long)]
+    sample_size: u32,
+
+    /// The maximum number of records to read/sample from
+    #[structopt(short, long)]
+    max_number_to_sample_from: Option<u32>,
+
+    /// The minimum length of sequences to include in the sample
+    #[structopt(short = "l", long)]
+    min_sequence_length: Option<u32>,
+
+    /// Replace sequence identifiers and descriptions in record header with
+    /// the record number, optionally prefixed with the dataset identifier
+    /// if specified
+    #[structopt(short, long)]
+    replace_sequence_ids: bool,
+
+    /// Summary of the numbers of records read and sampled
+    #[structopt(short, long)]
+    summary_file: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let config = Config::from_args();
+
+    let (mut records, total) = sample_fastq(
+        &config.fastq_files,
+        config.sample_size,
+        config.max_number_to_sample_from,
+        config.min_sequence_length,
+    )?;
+
+    if config.replace_sequence_ids {
+        rename_sequence_ids(&mut records, &config.id);
+    }
+
+    write_fastq_records(&records, &config.output_file)?;
+
+    if let Some(summary_file) = config.summary_file {
+        write_summary(&config.id, total, records.len() as u32, &summary_file)?;
+    }
+
+    Ok(())
+}
+
+fn sample_fastq(
+    fastq_files: &[PathBuf],
+    sample_size: u32,
+    max_number_to_sample_from: Option<u32>,
+    min_sequence_length: Option<u32>,
+) -> Result<(Vec<FastqRecord>, u32)> {
+    let mut sampled_records = Vec::new();
+    let mut number_of_records_read = 0;
+
+    let mut rng = thread_rng();
+
+    // loop over and sample from input FASTQ files using reservoir sampling
+    for fastq_file in fastq_files {
+        let filename = fastq_file.to_str().unwrap();
+        info!("Reading {}", filename);
+
+        let mut reader = create_fastq_reader(fastq_file)?;
+
+        let mut count = 0;
+        let mut record = FastqRecord::new();
+
+        while reader.read_next_into(&mut record)? {
+            count += 1;
+            number_of_records_read += 1;
+
+            if count % 1_000_000 == 0 {
+                info!("{}", count);
+            }
+
+            if let Some(min_sequence_length) = min_sequence_length {
+                if record.seq.len() < (min_sequence_length as usize) {
+                    continue;
+                }
+            }
+
+            if sampled_records.len() < sample_size as usize {
+                sampled_records.push(record.clone());
+            } else {
+                let index = rng.gen_range(0..number_of_records_read);
+                if index < sample_size {
+                    sampled_records[index as usize] = record.clone();
+                }
+            }
+
+            if let Some(max_number_to_sample_from) = max_number_to_sample_from {
+                if count > max_number_to_sample_from {
+                    break;
+                }
+            }
+        }
+
+        if count % 1_000_000 != 0 {
+            info!("{}", count);
+        }
+    }
+
+    info!("Total records read: {}", number_of_records_read);
+    info!("Number of records sampled: {}", sampled_records.len());
+
+    Ok((sampled_records, number_of_records_read))
+}
+
+fn rename_sequence_ids(records: &mut Vec<FastqRecord>, prefix: &Option<String>) {
+    info!("Renaming sequence identifiers");
+    let mut count = 0;
+    for record in records.iter_mut() {
+        count += 1;
+        match prefix {
+            Some(id) => record.id = format!("{}:{}", id, count),
+            None => record.id = count.to_string(),
+        }
+        record.desc = None;
+    }
+}
+
+fn write_fastq_records(records: &[FastqRecord], output_file: &Option<PathBuf>) -> Result<()> {
+    match output_file {
+        Some(file) => info!(
+            "Writing sampled FASTQ records to {}",
+            file.to_str().unwrap()
+        ),
+        None => info!("Writing sampled FASTQ records to stdout"),
+    }
+
+    let mut writer = create_fastq_writer(output_file)?;
+
+    // write sampled records
+    let mut count = 0;
+    for record in records {
+        count += 1;
+        writer.write_fastq(record)?;
+        if count % 1_000_000 == 0 {
+            info!("{}", count);
+        }
+    }
+
+    if count % 1_000_000 != 0 {
+        info!("{}", count);
+    }
+
+    writer.flush()?;
+
+    Ok(())
+}
+
+#[derive(Debug, Serialize)]
+struct Summary {
+    id: String,
+    read: u32,
+    sampled: u32,
+}
+
+fn write_summary(
+    id: &Option<String>,
+    read: u32,
+    sampled: u32,
+    summary_file: &PathBuf,
+) -> Result<()> {
+    let summary_filename = summary_file.to_str().unwrap();
+    info!("Writing summary to {}", summary_filename);
+    let id = match id {
+        Some(id) => id.clone(),
+        None => String::from(""),
+    };
+    let summary = Summary { id, read, sampled };
+    let mut summary_writer = csv::Writer::from_path(summary_file)
+        .with_context(|| format!("Error creating summary file {}", summary_filename))?;
+    summary_writer
+        .serialize(summary)
+        .with_context(|| format!("Error writing summary to {}", summary_filename))?;
+    summary_writer.flush().with_context(|| {
+        format!(
+            "Error writing summary file {} to completion",
+            summary_filename
+        )
+    })?;
+
+    Ok(())
+}
---
+++ src/bin/trim_and_split_fastq.rs
@@ -0,0 +1,141 @@
+use anyhow::{bail, Result};
+use log::info;
+use mga::fastq::{create_fastq_reader, create_fastq_writer, FastqRecord};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// Trim sequences from FASTQ files and split into chunks in FASTQ and/or FASTA
+/// format
+#[derive(StructOpt)]
+struct Config {
+    /// FASTQ file to sample records from
+    #[structopt(parse(from_os_str))]
+    fastq_files: Vec<PathBuf>,
+
+    /// The prefix for output file names
+    #[structopt(short, long)]
+    output_prefix: String,
+
+    /// The chunk size for splitting records into separate files
+    #[structopt(short = "n", long, default_value = "1000000")]
+    chunk_size: u32,
+
+    /// The start position in reads (preceeding bases will be trimmed)
+    #[structopt(short, long)]
+    start: Option<usize>,
+
+    /// The maximum length of trimmed sequences
+    #[structopt(short, long)]
+    length: Option<usize>,
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let config = Config::from_args();
+
+    if config.chunk_size == 0 {
+        bail!("Invalid chunk size");
+    }
+    if let Some(start) = config.start {
+        if start == 0 {
+            bail!("Invalid start position for trimming - numbering is 1-based");
+        }
+    }
+
+    if let Some(length) = config.length {
+        if length == 0 {
+            bail!("Zero length specified for trimmed sequences");
+        }
+    }
+
+    trim_and_split(
+        &config.fastq_files,
+        &config.output_prefix,
+        config.chunk_size,
+        config.start,
+        config.length,
+    )?;
+
+    Ok(())
+}
+
+fn trim_and_split(
+    fastq_files: &[PathBuf],
+    output_prefix: &str,
+    chunk_size: u32,
+    start: Option<usize>,
+    length: Option<usize>,
+) -> Result<()> {
+    let mut count = 0;
+    let mut filtered_count = 0;
+
+    let mut output_file_count = 1;
+    let output_fastq_file = format!("{}.{}.fq.gz", output_prefix, output_file_count);
+    info!("Creating {}", output_fastq_file);
+    let output_fastq_path = Some(PathBuf::from(output_fastq_file));
+    let mut fastq_writer = create_fastq_writer(&output_fastq_path)?;
+    let mut chunk_count = 0;
+
+    for fastq_file in fastq_files {
+        let filename = fastq_file.to_str().unwrap();
+        info!("Reading {}", filename);
+
+        let mut fastq_reader = create_fastq_reader(fastq_file)?;
+
+        let mut record = FastqRecord::new();
+
+        while fastq_reader.read_next_into(&mut record)? {
+            // check whether we need to create a new chunk file
+            if chunk_count > 0 && chunk_count % chunk_size == 0 {
+                fastq_writer.flush()?;
+                output_file_count += 1;
+                let output_fastq_file = format!("{}.{}.fq.gz", output_prefix, output_file_count);
+                info!("Creating {}", output_fastq_file);
+                let output_fastq_path = Some(PathBuf::from(output_fastq_file));
+                fastq_writer = create_fastq_writer(&output_fastq_path)?;
+            }
+
+            match length {
+                Some(length) => match start {
+                    Some(start) => record.trim(start, Some(start + length - 1))?,
+                    None => record.trim_to_length(length)?,
+                },
+                None => {
+                    if let Some(start) = start {
+                        record.trim(start, None)?;
+                    }
+                }
+            }
+
+            if record.seq.is_empty() {
+                filtered_count += 1;
+            } else {
+                chunk_count += 1;
+                fastq_writer.write_fastq(&record)?;
+            }
+
+            count += 1;
+            if count % 1_000_000 == 0 {
+                info!("{}", count);
+            }
+        }
+    }
+
+    if count % 1_000_000 != 0 {
+        info!("{}", count);
+    }
+
+    info!(
+        "{} of {} records filtered after trimming",
+        filtered_count, count
+    );
+
+    fastq_writer.flush()?;
+
+    if filtered_count == count {
+        bail!("No sequences remain after trimming and zero-length sequences filtered");
+    }
+
+    Ok(())
+}
