94b005c652f74b76a7d367922cec283e5a06dd72
eldrid01 <matthew.d.eldridge@gmail.com>
2021-02-25 11:41:33 +0000

    Read list of genomes for which bowtie indexes exist and add to synonym-based lookup, added checks that specified species/controls actually match to a genome

--- R/check_inputs.R
+++ R/check_inputs.R
@@ -1,12 +1,15 @@
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 3)
 {
-  stop('Usage: Rscript check_inputs.R samples_file genomes_file output_file')
+  stop('Usage: Rscript check_inputs.R samples_file genomes_file genome_list_file output_file')
 }
+
+args <- c("samplesheet.csv", "genomes.csv", "genome_list.txt", "samples.validated.csv")
 
 samples_file <- args[1]
 genomes_file <- args[2]
-output_file <- args[3]
+genome_list_file <- args[3]
+output_file <- args[4]
 
 suppressPackageStartupMessages(library(tidyverse))
 
@@ -31,10 +34,16 @@ if (length(missing_columns) > 0) {
   stop("Missing columns in ", genomes_file, " (", str_c(missing_columns, collapse = ", "), ")")
 }
 
+# read list of genomes with bowtie indexes
+genome_list <- read_tsv(genome_list_file, col_names = "genome", col_types = "c")
+
+# add missing genomes in the genomes file for which we have bowtie indexes
+genomes <- full_join(genomes, genome_list, by = "genome")
+
 # create synonym lookup table
 synonyms <- bind_rows(
   genomes %>%
-    select(genome, synonym = genome),
+    select(genome = genome, synonym = genome),
   genomes %>%
     select(genome, synonym = species),
   genomes %>%
@@ -60,9 +69,33 @@ if (nrow(duplicate_synonyms) > 0) {
   stop("Error: ", genomes_file, " contains the same synonym(s) for more than one genome")
 }
 
-samples %>%
+# match species and controls to genomes
+samples <- samples %>%
   left_join(synonyms, by = c("species" = "synonym")) %>%
-  left_join(select(synonyms, control = synonym, `control genome` = genome), by = "control") %>%
-  mutate(id_prefix = row_number()) %>%
-  write_csv(output_file)
+  left_join(select(synonyms, control = synonym, `control genome` = genome), by = "control")
 
+# check for species that don't match to a genome
+non_matching_species <- samples %>%
+  filter(!is.na(species)) %>%
+  filter(is.na(genome)) %>%
+  distinct(species) %>%
+  pull(species)
+if (length(non_matching_species) > 0) {
+  stop("no genome match for the following species: '", str_c(non_matching_species, collapse = "', '"), "'")
+}
+
+# check for controls that don't match to a genome
+non_matching_control <- samples %>%
+  filter(!is.na(control)) %>%
+  filter(is.na(`control genome`)) %>%
+  distinct(control) %>%
+  pull(control)
+if (length(non_matching_control) > 0) {
+  stop("no genome match for the following controls: '", str_c(non_matching_control, collapse = "', '"), "'")
+}
+
+# add unique numeric identifier to be used in labeling sampled reads
+samples <- mutate(samples, id_prefix = row_number())
+
+# write new sample sheet
+write_csv(samples, output_file)
