c3295a5c0ae1e730e626194d1a0f8aff0d935ad5
eldrid01 <matthew.d.eldridge@gmail.com>
2021-03-28 19:02:09 +0100

    Remove plotting code, now handled by separate R script

--- bin/summarize_alignments.R
+++ bin/summarize_alignments.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-## summarize alignments and creates stacked bar chart plot
+# create alignment summaries
 
 suppressPackageStartupMessages(library(optparse))
 
@@ -433,70 +433,3 @@ alignment_summary %>%
   rename(id = name) %>%
   mutate(across(where(is.logical), ifelse, "yes", "no")) %>%
   write_csv(output_alignment_summary_file, na = "")
-
-
-# stacked bar chart
-# -----------------
-
-message("Creating bar chart")
-
-lower_error_rate <- 0.25
-upper_error_rate <- 1.5
-
-plot_data <- alignment_summary %>%
-  select(id, name, reference = genome, count = assigned, error_rate = `assigned error rate`, expected, control) %>%
-  filter(count > 0) %>%
-  mutate(category = ifelse(expected, "expected species", "unexpected/contaminant")) %>%
-  mutate(category = ifelse(control, "control", category)) %>%
-  mutate(category = replace_na(category, "unmapped")) %>%
-  select(-expected, -control) %>%
-  mutate(type = "genome") %>%
-  bind_rows(transmute(summary, id, name, reference = "adapter", count = adapter, error_rate = lower_error_rate, category = "adapter", type = "adapter")) %>%
-  left_join(counts, by = "id") %>%
-  mutate(scaled_count = ifelse(sampled == 0, 0, (count / sampled) * sequences / 1e6)) %>%
-  select(-sampled, -sequences) %>%
-  arrange(id) %>%
-  mutate(name = as_factor(name)) %>%
-  mutate(type = factor(type, levels = c("adapter", "genome"))) %>%
-  mutate(category = factor(category, levels = rev(c("expected species", "control", "unexpected/contaminant", "unmapped", "adapter")))) %>%
-  mutate(bounded_error_rate = pmin(error_rate, upper_error_rate, na.rm = TRUE)) %>%
-  mutate(bounded_error_rate = pmax(bounded_error_rate, lower_error_rate, na.rm = TRUE)) %>%
-  mutate(bounded_error_rate = ifelse(category == "unmapped", lower_error_rate, bounded_error_rate)) %>%
-  arrange(name, type, category, count) %>%
-  mutate(group = row_number()) %>%
-  mutate(bar_width = ifelse(type == "adapter", 0.5, 1))
-
-plot <- ggplot(plot_data, aes(x = type, y = scaled_count, group = group, fill = category, alpha = -bounded_error_rate)) +
-  geom_col(colour = "grey30", width = plot_data$bar_width) +
-  scale_y_continuous(limits = c(0, NA), expand = expansion(mult = c(0, 0.05), add = 0), labels = scales::comma) +
-  scale_fill_manual(values = c("magenta", "grey97", "red", "goldenrod1", "green"), drop = FALSE, guide = guide_legend(reverse = TRUE)) +
-  scale_alpha(guide = "none") +
-  labs(y = "sequence reads (millions)") +
-  coord_flip() +
-  facet_grid(rows = vars(name), switch = "y") +
-  theme_bw() +
-  theme(
-    legend.position = "bottom",
-    legend.title = element_blank(),
-    legend.key.height = unit(0.8, "line"),
-    legend.key.width = unit(1.5, "line"),
-    legend.text = element_text(margin = margin(r = 1, unit = "line")),
-    panel.border = element_blank(),
-    panel.grid = element_blank(),
-    axis.line.x = element_line(),
-    axis.ticks.y = element_blank(),
-    axis.ticks.length.x = unit(0.35, "line"),
-    axis.title.x = element_text(size = 10),
-    axis.title.y = element_blank(),
-    axis.text.y = element_blank(),
-    strip.background = element_blank(),
-    strip.text.y.left = element_text(angle = 0, vjust = 0.8)
-  )
-
-width <- 10
-height <- 0.7 * (1.5 + nrow(sample_names))
-
-ggsave(str_c(output_plot_file_prefix, ".png"), plot, width = width, height = height)
-ggsave(str_c(output_plot_file_prefix, ".pdf"), plot, width = width, height = height)
-ggsave(str_c(output_plot_file_prefix, ".svg"), plot, width = width, height = height)
-
