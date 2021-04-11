9796629c24312571683962e0e73f3054655b0e18
eldrid01 <matthew.d.eldridge@gmail.com>
2021-04-11 06:48:58 +0100

    Added MGA context for the crate in the documentation comment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! A Rust library providing functions for reading, sampling, trimming
-//! and writing sequence data in FASTQ and FASTA formats.
+//! and writing sequence data in FASTQ and FASTA formats to support the
+//! Multi-Genome Alignment (MGA) contaminant screen pipeline.
 
 pub mod fastq;
