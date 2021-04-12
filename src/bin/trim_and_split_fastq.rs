e38ce9acaad72094355096240e6b3b1beddeb8b6
eldrid01 <matthew.d.eldridge@gmail.com>
2021-04-12 12:23:17 +0100

    Added copyright notice

--- src/bin/trim_and_split_fastq.rs
+++ src/bin/trim_and_split_fastq.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2021 CRUK Cambridge Institute - Bioinformatics Core
+// Licensed under the MIT license (http://opensource.org/licenses/MIT)
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! Trim sequences from FASTQ files and split into chunks in FASTQ and/or FASTA
 //! format.
 
