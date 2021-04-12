5e33305158db0a199f7b9634973c2705599f30c0
eldrid01 <matthew.d.eldridge@gmail.com>
2021-04-12 12:30:52 +0100

    Added copyright notice

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2021 CRUK Cambridge Institute - Bioinformatics Core
+// Licensed under the MIT license (http://opensource.org/licenses/MIT)
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! A Rust library providing functions for reading, sampling, trimming
 //! and writing sequence data in FASTQ and FASTA formats to support the
 //! Multi-Genome Alignment (MGA) contaminant screen pipeline.
